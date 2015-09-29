e7bd2fde9f12a7f4af4d63e0cfe42f9b2d821d2c
Joel Scoble <joel.scoble@outlook.com>
2015-09-29 00:17:30 -0500

    fmtd

--- ersatzjson.go
+++ ersatzjson.go
@@ -1,15 +1,19 @@
 package ersatzjson
 
 import (
-  "encoding/json"
+	"encoding/json"
 
-  "github.com/mohae/nocomment"
+	"github.com/mohae/nocomment"
 )
+
+type Decoder struct {
+	json.Decoder
+}
 
 // Unmarshal elides comments from the "json" and then calls
 // json.Unmarshal to unmarshal to the provided interface{}.
 func Unmarshal(data []byte, v interface{}) error {
-  data = nocomment.Clean(data)
-  err := json.Unmarshal(data, &v)
-  return err
+	data = nocomment.Clean(data)
+	err := json.Unmarshal(data, &v)
+	return err
 }
